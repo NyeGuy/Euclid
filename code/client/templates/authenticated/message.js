c739e908fb9fbe0b2c3e7154b30f7a69020a317e
N P <pnamasondhi@gmail.com>
2016-09-09 19:45:54 -0700

    edited message template and template handlers

--- code/client/templates/authenticated/message.js
+++ code/client/templates/authenticated/message.js
@@ -1,15 +1,35 @@
 Template.message.helpers({
   name( userId ) {
-    if ( userId ) {
+    if ( userId && userId != "Doofus") {
       let user = Meteor.users.findOne( userId, { fields: { 'profile.name': 1 } } );
       return user ? `${ user.profile.name.first } ${ user.profile.name.last }` : '';
     }
+    return "GameBot";
+  },
+
+  // block(blockId) {
+  //   return 
+  // }
+  
+  messageFromGameBot(message){
+    if (message.owner === "GameBot"){
+      return true;
+    } else {
+      return false;
+    }
   }
+
 });
 
 Template.message.events({
   'click a' ( event ) {
     event.preventDefault();
     window.open( event.target.href, '_blank' );
+  },
+
+  //convert this to select the next appropriate block in conversation.
+  'click button' () {
+    alert("test");
   }
+
 });
