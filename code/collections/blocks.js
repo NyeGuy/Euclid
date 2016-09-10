f6a0d9df25ed136b071b4c47e3067a309b75ade6
N P <pnamasondhi@gmail.com>
2016-09-10 11:32:59 -0700

    added MongoDB collection for blocks object

---
+++ code/collections/blocks.js
@@ -0,0 +1,15 @@
+Blocks = new Mongo.Collection('blocks');
+
+// Blocks.allow({
+//   insert: () => true,
+//   update: () => true,
+//   remove: () => true
+// });
+
+// Blocks.deny({
+//   insert: () => false,
+//   update: () => false,
+//   remove: () => false
+// });
+
+
