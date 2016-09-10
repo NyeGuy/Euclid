790ddf3afea6f0fce8d57af9d4b42a51b4f9f099
N P <pnamasondhi@gmail.com>
2016-09-09 19:35:55 -0700

    modified test accounts

--- code/server/modules/seed-database.js
+++ code/server/modules/seed-database.js
@@ -19,7 +19,31 @@ let _seedUsers = () => {
         name: { first: 'Doug', last: 'Funnie' }
       },
       roles: [ 'admin' ]
-    }]
+    },{
+      username: 'Euclid',
+      email: 'euclid@gamebot.com',
+      password: 'secretkey',
+      profile: {
+        name: { first: 'Doug', last: 'Funnie' }
+      },
+      roles: [ 'bot' ]
+    },{
+      username: 'TestUser',
+      email: 'euclid@gamebot2.com',
+      password: 'secretkey2',
+      profile: {
+        name: { first: 'Doug', last: 'Funnie' }
+      },
+      roles: [ 'bot' ],
+      lives: 5,
+      xp: 1000,
+      bits: 2000
+    }
+
+
+
+
+    ]
   });
 };
 
@@ -30,7 +54,60 @@ let _seedChannels = () => {
   });
 };
 
+let _seedBlocks = () => {
+  Seed( 'blocks', {
+    environments: [ 'development', 'staging', 'production' ],
+    data: [ 
+      { 
+        blockNum: 1,
+        image:"",
+        description: 'Welcome to GameBots',
+        button1: "True",
+        button1Next: 2,
+        button2: "False",
+        button1Next: 3,
+        button3: "Skip",
+        button1Next: 4
+      },
+      { 
+        blockNum: 2,
+        image:"",
+        description: 'Question 1',
+        button1: "True",
+        button1Next: 2,
+        button2: "False",
+        button1Next: 3,
+        button3: "Skip",
+        button1Next: 4
+      },
+      { 
+        blockNum: 3,
+        image:"",
+        description: 'Question 2',
+        button1: "True",
+        button1Next: 2,
+        button2: "False",
+        button1Next: 3,
+        button3: "Skip",
+        button1Next: 4
+      },
+      { 
+        blockNum: 4,
+        image:"",
+        description: 'Question 3',
+        button1: "True",
+        button1Next: 2,
+        button2: "False",
+        button1Next: 3,
+        button3: "Skip",
+        button1Next: 4
+      }
+    ]
+  });
+}
+
 export default function() {
   _seedUsers();
   _seedChannels();
+  _seedBlocks();
 }
