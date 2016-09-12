15c72e28dc1b11195dea6f62e916270509a5a217
N P <pnamasondhi@gmail.com>
2016-09-11 17:57:01 -0700

    updated user query object in template handler

--- code/client/templates/authenticated/profilecard.js
+++ code/client/templates/authenticated/profilecard.js
@@ -1,24 +1,17 @@
+Template.sidebar.onCreated( () => {
+  let template = Template.instance();
+  template.subscribe( 'profilecard' );
+});
+
 Template.profilecard.helpers({
 	
 	user(){
-
-		const whoAmI = Meteor.subscribe('users.public');
-		console.log(whoAmI);
-		return whoAmI;
-		// return Players.find({_id: user._id});
-	  //let user = Meteor.users.findOne( { username: username } );
-	  // console.log(Meteor.users.findOne({_id: Meteor.userId() }));
-	  // console.log(Meteor.users.find({_id: Meteor.userId() }).fetch()[0]);
-	  // return Meteor.users.find({_id: Meteor.userId() });
-	  // return Meteor.users.find({email: "euclid@gamebot2.com" });
-	},
+	  return (Meteor.users.find({_id: Meteor.userId() }).fetch()[0])
+	}
 
 });
 
 Template.profilecard.events({
 	"mouseover #user-stats"(){
-		// console.log(Meteor.users.findOne({_id: Meteor.userId() }))
-
 	}
-
 });
